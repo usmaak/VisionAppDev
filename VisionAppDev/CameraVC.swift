13c51143f63d64ffb0f028d78204d33f3585e131
Scott <usmaak@ameritech.net>
2017-12-07 19:38:51 -0700

    Trying to get this in github

--- VisionAppDev/CameraVC.swift
+++ VisionAppDev/CameraVC.swift
@@ -17,7 +17,6 @@ class CameraVC: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
